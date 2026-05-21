2ac0f876fec3bfdb3faba1c3aa09803ac9cd070f
XinQingQwQ <124947648+XingChenwa@users.noreply.github.com>
2026-05-21 20:54:10 +0800

    Update sidebar.ts
    Signed-off-by: XinQingQwQ <124947648+XingChenwa@users.noreply.github.com>

--- .vitepress/script/sidebar.ts
+++ .vitepress/script/sidebar.ts
@@ -11,7 +11,7 @@ const sidebar = [
     items: [
       {
         text: '<i class="fa-solid fa-bars fa-flip"></i> HGadmin-v3 v3说明', link: '/docs/intro/main/main', items: [
-          { text: '<i class="fa-solid fa-gear fa-spin"></i> 配置文件', link: '/docs/intro/main/HGadmin-v3v3config', },
+          { text: '<i class="fa-solid fa-gear fa-spin"></i> 配置文件', link: '/docs/intro/main/hgadminv3config', },
           { text: '<i class="fa-brands fa-modx fa-fade"></i> 模块化传送系统相关', link: '/docs/intro/main/csmk' },
         ]
       },
